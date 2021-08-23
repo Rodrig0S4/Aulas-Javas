7d24cf581933ef04b89fd38f84a706ff14dc1aa6
unknown <sdearaujo.rodrigo@gmail.com>
2021-08-23 16:28:07 -0300

    Atualização do Spring Boot

--- Spring Boot 1 Aula/src/main/java/com/primario/primeiro/controller/PrimeiroController.java
+++ Spring Boot 1 Aula/src/main/java/com/primario/primeiro/controller/PrimeiroController.java
@@ -16,14 +16,17 @@ public class PrimeiroController {
 	@GetMapping("/first")
 	public String first() {
 		
-		return "First conception!!! ";
+		return "</br>Mentalidades:</br>1-Mentalidade de Crescimento.</br>2-Persistência."
+				+"</br>3-Orientação ao Futuro."+"</br>Habilidades:</br>1-Atenção aos detalhes."
+						+"</br>2-Trabalho em equipe.";
 			
 	}
 	
 	@GetMapping("/first2")
 	public String first2() {
 		
-		return "First conception 2!!!";
+		return "</br>Meus objetivos de aprendizagem para está semana:"+
+				 "</br>1-Conseguir dominar o conceito de Spring Boot.</br>2-Aprender a criar meu próprio\"Maven\", sem auxilio de ferramentas online.";
 		
 	}
 	
